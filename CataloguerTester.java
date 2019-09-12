71e3ed6791b3c84dafb8235a3ab2c82356c520c0
Johanna Nelson <johannanelson99@gmail.com>
2019-09-12 00:19:30 -0400

    Update CataloguerTester.java

--- CataloguerTester.java
+++ CataloguerTester.java
@@ -4,15 +4,15 @@ import java.util.*;
 public class CataloguerTester {
 
     public static void main(String[] args){
-
-
- //BookCataloguer e = new BookCataloguer(args[1]);
+        
        try {
             System.out.println("\n*********************************************************************");
             System.out.println("BOOK CATALOGUER");
             System.out.println("*********************************************************************");
             System.out.println("This program creates a catalog of any book's diction – you can either view a list of all words used, or search for a specific word. If this is your first time using this program, I recommend choosing 'odyssey.txt' and searching for the usages of 'swift ship'.\n");
             System.out.print("-----> Type a txt filename to proceed (eg. odyssey.txt): ");
+            
+            // Scan book and choice selections
             Scanner s = new Scanner(System.in);
             String book = s.nextLine();
             BookCataloguer e = new BookCataloguer(book);
@@ -27,6 +27,8 @@ public class CataloguerTester {
                if (option.equals("c")) { System.exit(1); }
             }
         }
+        
+        // args exception
         catch (Exception ex) {
               if (args[0] == null) {
                   System.out.println("\n\nTO RUN CORRECTLY, type 'java CataloguerTester TEXTFILE.txt'\n");
