23adeefa899adf717e4acac95bc116631b21b363
Johanna Nelson <johannanelson99@gmail.com>
2019-09-12 00:18:32 -0400

    Update CataloguerTester.java

--- CataloguerTester.java
+++ CataloguerTester.java
@@ -4,18 +4,23 @@ import java.util.*;
 public class CataloguerTester {
 
     public static void main(String[] args){
-      BookCataloguer e;
+
 
  //BookCataloguer e = new BookCataloguer(args[1]);
        try {
-            System.out.print("\nThis program creates a catalog of any book's diction – you can either view a list of all words used, or search for a specific word. If this is your first time using this program, I recommend choosing 'odyssey.txt' and searching for the usages of 'ulysses'.\n\n");
-            e = new BookCataloguer(args[0]);
+            System.out.println("\n*********************************************************************");
+            System.out.println("BOOK CATALOGUER");
+            System.out.println("*********************************************************************");
+            System.out.println("This program creates a catalog of any book's diction – you can either view a list of all words used, or search for a specific word. If this is your first time using this program, I recommend choosing 'odyssey.txt' and searching for the usages of 'swift ship'.\n");
+            System.out.print("-----> Type a txt filename to proceed (eg. odyssey.txt): ");
             Scanner s = new Scanner(System.in);
+            String book = s.nextLine();
+            BookCataloguer e = new BookCataloguer(book);
             while (true) {
-               System.out.println("You've chosen " + args[0] + ". Type: ");
-               System.out.println("      'a' to search for a word or phrase");
-               System.out.println("      'b' to see a catalog of the book by number of occurrences");
-               System.out.println("      'c' to exit program");
+               System.out.println("\nYou've chosen " + book + ". Type: ");
+               System.out.println("------->'a' to search for a word or phrase");
+               System.out.println("------->'b' to see a catalog of the book by number of occurrences");
+               System.out.println("------->'c' to exit program");
                String option = s.nextLine();
                if (option.equals("a")) { e.epithetCounter(); }
                if (option.equals("b")) { e.hashTheText(); }
